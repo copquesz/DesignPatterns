cd252a3b8d5dea6edbcb2b49aa5ceaab3f008b94
Lucas Copque <copque.faculdade@gmail.com>
2018-06-06 15:40:23 -0300

    Design Patterns Estruturais

---
+++ src/adapter/map/Main.java
@@ -0,0 +1,23 @@
+package adapter.map;
+
+import java.util.Map;
+
+/**
+ * 
+ * @author Lucas Copque - RA: 816112862
+ *
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		Object[][] pares = new String[][] { { "Palmeiras", "Corinthians", "São Paulo", "Flamengo", "Vasco", "Fluminense" },
+				{ "Campeão", "Ladrão", "Série B", "Lixo", "Falido", "Fraco" } };
+
+		Map<?, ?> mapa = new NewMapAdapter(pares);
+		
+		for(Object key : mapa.keySet()) {
+			System.out.println(key + " : " + mapa.get(key));
+		}
+	}
+
+}
